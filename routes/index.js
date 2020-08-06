946bd84ebbb22eae69338e5f562aa0a249c2a6cb
Julio Martinez <jmartinez@jacidi.com>
2020-08-06 19:47:54 -0400

    Update calculator

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,30 @@ router.get('/', function (req, res, next) {
   
   let numero1 = req.query.numero1;
   let numero2 = req.query.numero2;
+  let tipo = req.query.tipo;
+  let resultado = 0;
+  let error = "";
 
-  let resultado = parseInt(numero1) + parseInt(numero2);
+  if (tipo === 'suma') {
+  /* Logica */
+    resultado = parseInt(numero1) + parseInt(numero2);
+  } else if (tipo === 'resta') {
+  /* logica */
+    resultado = parseInt(numero1) - parseInt(numero2);
+  } else if (tipo === 'multi') {
+  /* logica */
+    resultado = parseInt(numero1) * parseInt(numero2);
+  } else if (tipo === 'divi') {
+  /* logica */
+    resultado = parseInt(numero1) / parseInt(numero2);
+  } else {
+  /* logica */
+    error = "Tipo invalido"
+  }
 
-  res.render('index', { numero: resultado });
+  console.log(tipo);
+
+  res.render('index', { resultado: resultado, error: error });
 });
 
 module.exports = router;
